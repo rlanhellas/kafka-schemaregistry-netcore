946d1732835ea4cdaa344f1b595b240d36732599
ronaldo.lanhellas <ronaldo.lanhellas@gmail.com>
2019-11-30 14:10:26 -0300

    creating project

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace kafka_schemaregistry_ssl_netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartConsumer();
+        }
+
+        private static void StartConsumer()
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "netcore-consumer-test-02",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = false,
+                SslCertificateLocation =  "/home/ronaldo.lanhellas/Workspace/tools/kafka/certs/client.crt",
+                SslKeyLocation = "/home/ronaldo.lanhellas/Workspace/tools/kafka/certs/client.key",
+                SslCaLocation = "/home/ronaldo.lanhellas/Workspace/tools/kafka/certs/rootCA.crt",
+                SecurityProtocol = SecurityProtocol.Ssl
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(config).Build())
+            {
+                consumer.Subscribe("netcore-topic-test");
+                CancellationTokenSource tokenSource = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    tokenSource.Cancel();
+                };
+
+                try
+                {
+                    while(true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(tokenSource.Token);
+                            Console.WriteLine($"Key={cr.Key}, Value={cr.Value}");
+                            // consumer.Commit();
+                        }catch(ConsumeException e)
+                        {
+                            Console.WriteLine($"Error {e.Error.Reason}");
+                        }
+                    }
+                }catch (OperationCanceledException)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
